7ec921055e138f5fed19295d1ed92cee8a79307f
David Viejo <dviejo@kungfusoftware.es>
2026-03-31 11:02:19 +0200

    Update temps to v0.0.8

--- Formula/temps.rb
+++ Formula/temps.rb
@@ -1,23 +1,23 @@
 class Temps < Formula
   desc "Temps - Temporary file and directory management tool"
   homepage "https://github.com/gotempsh/temps"
-  version "0.0.5"
+  version "0.0.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gotempsh/temps/releases/download/v0.0.5/temps-darwin-arm64.tar.gz"
-      sha256 "81a793a241c615f01c2a51bbc4d6f07fb7ca7546e12b3954d8145cf21d55796d"
+      url "https://github.com/gotempsh/temps/releases/download/v0.0.8/temps-darwin-arm64.tar.gz"
+      sha256 "02ab48ac462d999be6d9452cd134341768405645e0165d55f000e2ce3f3d9408"
     else
-      url "https://github.com/gotempsh/temps/releases/download/v0.0.5/temps-darwin-amd64.tar.gz"
-      sha256 "cf6330208b7aecec0398237277042e48535472addca3115f1182c7f799faab4a"
+      url "https://github.com/gotempsh/temps/releases/download/v0.0.8/temps-darwin-amd64.tar.gz"
+      sha256 "8deaad6746fc7e06ce6f610fd17ffd9ed78bd763f6caddb919c4da9587267cab"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/gotempsh/temps/releases/download/v0.0.5/temps-linux-amd64.tar.gz"
-      sha256 "3bdeab6e83489c5b260a6e7f0c587d35c29e2dee835a1fb8243d66086068465b"
+      url "https://github.com/gotempsh/temps/releases/download/v0.0.8/temps-linux-amd64.tar.gz"
+      sha256 "3a9945543a0c0b985af643c621cb9872baaf62d1be3a42a52d95ef93e44fded3"
     end
   end
 
